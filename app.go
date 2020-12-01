b47a07db13e4587ca015ce6d1f35cdb39ffc0c96
geomin76 <geom@vt.edu>
2020-11-30 20:36:07 -0500

    Added email code

--- app.go
+++ app.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/smtp"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func email(w http.ResponseWriter, r *http.Request) {
+	auth := smtp.PlainAuth("", "email", "pass", "smtp.gmail.com")
+
+	// Here we do it all: connect to our server, set up a message and send it
+	to := []string{""}
+	msg := []byte("Hello, World!")
+
+	err := smtp.SendMail("smtp.gmail.com:587", auth, "from", to, msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, "Email sent")
+}
+
+func text(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Text sent")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/email", email)
+	http.HandleFunc("/text", text)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
 
 func main() {
 	fmt.Println("Hello, World!")
+	fmt.Println("App is running at http://localhost:10000")
+	handleRequests()
 }
