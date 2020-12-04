d31e5bb5e3435468f58466dad98e13d01768cd3f
geomin76 <geom@vt.edu>
2020-12-04 14:13:47 -0500

    Adding permissions to script

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ GOOS=linux go build -o app app.go
 
 zip function.zip app
 
-aws lambda create-function --function-name messaging-server --runtime go1.x --zip-file fileb://function.zip --handler app --role $ROLE
+aws lambda create-function --function-name messaging-server --runtime go1.x --zip-file fileb://function.zip --handler main --role $ROLE
