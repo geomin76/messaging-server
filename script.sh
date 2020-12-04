a3299f46c919382a508f8262e67ac33019cf5266
geomin76 <geom@vt.edu>
2020-12-04 15:18:26 -0500

    Trying travis file

--- script.sh
+++ script.sh
@@ -12,4 +12,4 @@ GOOS=linux go build -o app app.go
 
 zip function.zip app
 
-aws lambda create-function --function-name messaging-server --runtime go1.x --zip-file fileb://function.zip --handler main --role $ROLE
+aws lambda create-function --function-name messaging-server --runtime go1.x --zip-file fileb://function.zip --region us-east-1 --handler main --role $ROLE
