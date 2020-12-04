34364c957347a24560752767584c39c53256dd4f
geomin76 <geom@vt.edu>
2020-12-04 16:18:31 -0500

    Trying travis file

--- script.sh
+++ script.sh
@@ -8,8 +8,8 @@ sudo ./aws/install
 
 aws --version
 
-GOOS=linux go build -o app app.go
+GOOS=linux go build -o main main.go
 
-zip function.zip app
+zip function.zip main
 
-aws lambda create-function --function-name messaging-server --runtime go1.x --zip-file fileb://function.zip --region us-east-1 --handler main --role arn:aws:iam::909857398854:role/messaging-server
+aws lambda update-function-code --function-name messaging-server --runtime go1.x --zip-file fileb://function.zip --region us-east-1 --handler main --role arn:aws:iam::909857398854:role/messaging-server
