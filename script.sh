68ec6399541d0642baada7cf10571b46912e69f2
geomin76 <geom@vt.edu>
2020-12-04 13:59:58 -0500

    Adding travis file

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+GOOS=linux go build -o app app.go
+
+zip function.zip app
+
+aws lambda create-function --function-name messaging-server --runtime go1.x --zip-file fileb://function.zip --handler app --role $ROLE
