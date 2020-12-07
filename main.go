cdfb12a3f9cfab530f8ad47bf6dc290e25a86c86
geomin76 <geom@vt.edu>
2020-12-07 00:38:24 -0500

    Updating file

--- main.go
+++ main.go
@@ -55,11 +55,25 @@ func text(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Text sent")
 }
 
+func determineListenAddress() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+	}
+	return ":" + port, nil
+}
+
 func main() {
+	addr, err := determineListenAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/email", email)
 	http.HandleFunc("/text", text)
-	fmt.Println("Hello, World!")
-	fmt.Println("App is running at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("Listening on %s...\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		panic(err)
+	}
 }
